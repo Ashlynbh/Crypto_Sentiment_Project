06e455dfb9ef12967e9ba16766265151af11868a
Ashlyn <ashlyn.bh@live.com.au>
2022-06-03 21:19:19 +0800

    updating html

--- results.js
+++ results.js
@@ -1,30 +1,24 @@
 
-var btcimg = document.getElementById("btc1");
-var btcimg2 = document.getElementById("btc2");
-var btcimg3 = document.getElementById("btc3");
-var btcimg4 = document.getElementById("btc4");
-var btcimg5 = document.getElementById("btc");
-var ethimg = document.getElementById("eth1");
-var ethimg2 = document.getElementById("eth2");
-var ethimg3 = document.getElementById("eth3");
-var ethimg4 = document.getElementById("eth4");
-var ethimg5 = document.getElementById("eth");
-var dogeimg = document.getElementById("doge1");
-var dogeimg2 = document.getElementById("doge2");
-var dogeimg3 = document.getElementById("doge3");
-var dogeimg4 = document.getElementById("doge4");
-var dogeimg5 = document.getElementById("doge");
-
+var btcimg = document.getElementById("btc5");
+var btcimg2 = document.getElementById("btc6");
+var btcimg3 = document.getElementById("btc7");
+// var btcimg4 = document.getElementById("btc8");
+var ethimg = document.getElementById("eth5");
+var ethimg2 = document.getElementById("eth6");
+var ethimg3 = document.getElementById("eth7");
+// var ethimg4 = document.getElementById("eth8");
+var dogeimg = document.getElementById("doge5");
+var dogeimg2 = document.getElementById("doge6");
+var dogeimg3 = document.getElementById("doge7");
+// var dogeimg4 = document.getElementById("doge8");
 ethimg.style.display = "none";
 ethimg2.style.display = "none";
 ethimg3.style.display = "none";
-ethimg4.style.display = "none";
-ethimg5.style.display = "none";
+// ethimg4.style.display = "none";
 dogeimg.style.display = "none";
 dogeimg2.style.display = "none";
 dogeimg3.style.display = "none";
-dogeimg4.style.display = "none";
-dogeimg5.style.display = "none";
+// dogeimg4.style.display = "none";
 
 function updatePlotly() {
 
@@ -46,18 +40,15 @@ function updatePlotly() {
     ethimg.style.display = "block";
     ethimg2.style.display = "block";
     ethimg3.style.display = "block";
-    ethimg4.style.display = "block";
-    ethimg5.style.display = "block";
+    // ethimg4.style.display = "block";
     btcimg.style.display = "none";
     btcimg2.style.display = "none";
     btcimg3.style.display = "none";
-    btcimg4.style.display = "none";
-    btcimg5.style.display = "none";
+    // btcimg4.style.display = "none";
     dogeimg.style.display = "none";
     dogeimg2.style.display = "none";
     dogeimg3.style.display = "none";
-    dogeimg4.style.display = "none";
-    dogeimg5.style.display = "none";
+    // dogeimg4.style.display = "none";
 
   }
   
@@ -66,18 +57,15 @@ function updatePlotly() {
     dogeimg.style.display = "block";
     dogeimg2.style.display = "block";
     dogeimg3.style.display = "block";
-    dogeimg4.style.display = "block";
-    dogeimg5.style.display = "block";
+    // dogeimg4.style.display = "block";
     btcimg.style.display = "none";
     btcimg2.style.display = "none";
     btcimg3.style.display = "none";
-    btcimg4.style.display = "none";
-    btcimg5.style.display = "none";
+    // btcimg4.style.display = "none";
     ethimg.style.display = "none";
     ethimg2.style.display = "none";
     ethimg3.style.display = "none";
-    ethimg4.style.display = "none";
-    ethimg5.style.display = "none";
+    // ethimg4.style.display = "none";
     
   };
 
